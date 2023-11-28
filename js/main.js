c80e4e17826a2d93744cc31153fb2a900dd21264
Mojde7th <mojde7th@gmail.com>
2023-11-28 07:51:21 +0330

    BaseConcepts

---
+++ js/main.js
@@ -0,0 +1,61 @@
+let x = 10;
+//alert(x);
+//alert("hii");
+console.log("x=", x);
+let data = { fname: "mojde", lname: "eb" };
+console.log(data);
+console.table(data);
+//alert(data.fname+" "+data.lname);
+console.log(document);
+console.log(typeof document);
+document.write("<h1>It is Written with document.write method</h1><hr>");
+document.querySelector(
+  "div#root"
+).innerHTML = `<h1 class="p-1">Hi this is with:  </h1> <strong class="bg-info p-1 ms-5 mt-5">document.querySelector("div#root").innerHTML</strong><hr>`;
+//comment
+/*kdkd
+ */
+var m = 90;
+console.log(m);
+var firstName = "mojde";
+var lastName = "eb";
+var fatherName = "re";
+var email = "mojde@gmail.com";
+var age = "30";
+var age2 = 40;
+console.log(firstName + " " + lastName + " is " + age);
+console.log(typeof firstName);
+console.log(typeof age2);
+let data2 = [];
+console.log(data2);
+console.log(typeof data2);
+console.log("data2 is let data2=[], typeof data2 is " + typeof data2);
+////////////////////////
+var firstName2 = "Mojde";
+var lastName2 = "eb";
+var flag = true;
+var check = false;
+var age = 32;
+var data3 = null;
+var x3 = 35.4;
+console.log(
+  "I defined var data3=null, data3 is: ",
+  data3,
+  "type of data3 is : ",
+  typeof data3
+);
+
+console.log(firstName2, typeof firstName2);
+console.log(lastName2, typeof lastName2);
+console.log(flag, typeof flag);
+console.log(check, typeof check);
+console.log(age, typeof age);
+console.log(data3, typeof data3);
+console.log(x3, typeof x3);
+var karim;
+console.log(
+  "I defined this: var karim and left it without setting value so karim is: ",
+  karim,
+  " type of karim is: " + typeof karim
+);
+console.log("I wrote this: console.log(5>10); and the result is: ", 5 > 10);
