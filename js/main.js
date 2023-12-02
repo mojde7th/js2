534b478db0c11f317fcfefbbc60d84c3a84c7728
Mojde7th <mojde7th@gmail.com>
2023-12-02 10:25:09 +0330

    dd

--- js/main.js
+++ js/main.js
@@ -1,61 +1,136 @@
-let x = 10;
-//alert(x);
-//alert("hii");
-console.log("x=", x);
-let data = { fname: "mojde", lname: "eb" };
-console.log(data);
-console.table(data);
-//alert(data.fname+" "+data.lname);
-console.log(document);
-console.log(typeof document);
-document.write("<h1>It is Written with document.write method</h1><hr>");
-document.querySelector(
-  "div#root"
-).innerHTML = `<h1 class="p-1">Hi this is with:  </h1> <strong class="bg-info p-1 ms-5 mt-5">document.querySelector("div#root").innerHTML</strong><hr>`;
-//comment
-/*kdkd
- */
-var m = 90;
-console.log(m);
-var firstName = "mojde";
-var lastName = "eb";
-var fatherName = "re";
-var email = "mojde@gmail.com";
-var age = "30";
-var age2 = 40;
-console.log(firstName + " " + lastName + " is " + age);
-console.log(typeof firstName);
-console.log(typeof age2);
-let data2 = [];
-console.log(data2);
-console.log(typeof data2);
-console.log("data2 is let data2=[], typeof data2 is " + typeof data2);
-////////////////////////
-var firstName2 = "Mojde";
-var lastName2 = "eb";
-var flag = true;
-var check = false;
-var age = 32;
-var data3 = null;
-var x3 = 35.4;
-console.log(
-  "I defined var data3=null, data3 is: ",
-  data3,
-  "type of data3 is : ",
-  typeof data3
-);
+let yyy = 700;
+let zzz = 60;
+console.log(yyy + zzz);
+console.log(String(yyy) + zzz);
+let bl = Boolean([]);
+let bl2 = Boolean({});
+console.log(bl);
+console.log(bl2);
+let h = 10;
 
-console.log(firstName2, typeof firstName2);
-console.log(lastName2, typeof lastName2);
-console.log(flag, typeof flag);
-console.log(check, typeof check);
-console.log(age, typeof age);
-console.log(data3, typeof data3);
-console.log(x3, typeof x3);
-var karim;
+let hh = 90;
+let result = h > hh;
+let result2 = h !== "10";
+console.log(typeof result);
+console.log(result2);
+let x = 10;
+let y = 20;
+console.log(Math.PI);
+console.log(Math.cos(Math.PI));
+console.log(Math.cos(Math.PI / 2));
+let sum = x + y;
+let sub = x - y;
+let mul = x * y;
+let div = x / y;
+let rem = x % y;
+let pow = x ** y;
 console.log(
-  "I defined this: var karim and left it without setting value so karim is: ",
-  karim,
-  " type of karim is: " + typeof karim
+  "*** sum of 10 and 20 is " +
+    sum +
+    "*** sub of 10 and 20 is " +
+    sub +
+    "*** mul of 10 and 20 is " +
+    mul +
+    "*** div of 10 and 20 is " +
+    div +
+    "*** rem of 10 and 20 is " +
+    rem +
+    "*** pow of 10 and 20 is " +
+    pow
 );
-console.log("I wrote this: console.log(5>10); and the result is: ", 5 > 10);
+x += y;
+console.log("x is 10 and y is 20 x+=y is x=10+20, so x is: ", x);
+x -= y;
+console.log("x is  now 30 and y is 20 x-=y is x=30-20, so x is: ", x);
+x *= y;
+console.log("x is now 10 and y is 20 x*=y is x=10*20, so x is: ", x);
+x /= y;
+console.log("x is now 200 and y is 20 x/=y is x=200/20, so x is: ", x);
+x %= y;
+console.log("x is now 10 and y is 20 x%=y is x=10%20, so x is: ", x);
+console.log(x / 5);
+let firstName = "turaj";
+let lastName = "armin";
+let fatherName = "ali";
+let age = 35;
+console.log("my name is:" + firstName + " and my age is " + age);
+console.log(lastName);
+console.log(fatherName);
+console.log(age);
+console.log(
+  `my name is ${firstName} and my lastname is ${lastName} and now backtick is used for concating ${age} and ${fatherName}`
+);
+console.log(
+  `${document.write(
+    'This is from template string with this comand: `${document.write("*****")}`'
+  )}`
+);
+let mypromt1 = prompt(
+  "My question is do you remember the timer?",
+  "My default answer which is like placeholder is yes"
+);
+let mypromt2 = prompt("your name:", ".....");
+console.log(
+  `${mypromt2} Your answer was: ${mypromt1} but from 46th charcter substring to end is:  ${mypromt1.substring(
+    46
+  )}`
+);
+console.log(
+  `typeof mypromt1: ${typeof mypromt1} and type of myprompt2 is ${typeof mypromt2}`
+);
+let xx = 10;
+let yy = 20;
+let sumsum = xx + yy;
+console.log(sumsum);
+xx = "30";
+yy = "40";
+let cas = xx + yy;
+console.log(cas);
+let t = "30";
+let k = +30;
+
+console.log(
+  `type of t let t= "30" is ${typeof t} and type of k let k=+30 is ${typeof k}`
+);
+let flt = "399.43";
+let parsflt = parseInt(flt);
+let parsflt2 = parseFloat(flt);
+let parssum = parsflt + parsflt2;
+console.log(parsflt);
+console.log(parsflt);
+console.log(parssum);
+console.log(
+  `my variable is let flt = "399.43"; ${flt} and with function let parsflt = parseInt(flt);  answer is ${parsflt} 399 and with let parsflt2 = parseFloat(flt); the result be: ${parsflt2} which is 399.43 and type of both: ${typeof parsflt} and ${typeof parsflt2}`
+);
+let v = prompt("x....", "---");
+console.log(
+  `let v=prompt("x....","---") and type of v is: ${typeof v} and when typeof Number(v) it is: ${typeof Number(
+    v
+  )}`
+);
+console.log(typeof Number(v));
+let xn = prompt("xn...!", "--");
+let yn = prompt("yn...!", "--");
+console.log("xn+yn is:" + xn + yn);
+let zn = +prompt("z...", "---");
+let zn2 = +prompt("zn2...", "---");
+console.log(`let zn = +prompt("z...", "---");
+let zn2 = +prompt("zn2...", "---"); then zn+zn2 is: ${zn + zn2}`);
+let Numberprompt1 = prompt("xx...!", "---");
+let Numberprompt2 = prompt("yy...!");
+console.log(`let Numberprompt1 = prompt("xx...!", "---");
+let Numberprompt2 = prompt("yy...!"); sum is:  ${
+  Numberprompt1 + Numberprompt2
+}`);
+let Numberprompt11 = Number(prompt("my num...!"));
+let Numberprompt22 = Number(prompt("yy...!"));
+console.log(
+  `let Numberprompt11 = Number(prompt("my num...!")); let Numberprompt22 = Number(prompt("yy...!")); sum is: ${
+    Numberprompt11 + Numberprompt22
+  }`
+);
+let txs = 0b11001111;
+console.log(t);
+
+let g = 0xff;
+console.log(g);
